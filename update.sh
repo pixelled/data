db9e94627e2d15803999491f19d6de7eed282d97
pixelled <pixelledliu@gmail.com>
2022-09-17 22:55:37 -0700

    Update

---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mongod &
+sleep 1
+echo -e \
+"use pixelled\n
+db.art.insertOne({ name: 't.png', width: 1920, height: 1080, date: new Date() })\n
+quit()" \
+| mongosh
+
+sleep .5
+mongoexport --collection=art --db=pixelled --out=images.json
+
+sleep .5
+echo -e \
+"use admin\n
+db.shutdownServer()\n
+quit()" \
+| mongosh
